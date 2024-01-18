f5d306c5fd13f5621aacbe6987c7ba1e5e97c88d
leonid Kozlovskiy <lenya.kozlovskiy.97@mail.ru>
2024-01-18 04:25:15 +0300

    feat:complete first task

---
+++ 01-read-file/index.js
@@ -0,0 +1,5 @@
+const fs = require('fs');
+const path = require('path');
+const pathFile = path.join(__dirname, 'text.txt');
+const readableStream = fs.createReadStream(pathFile);
+readableStream.on('data', (chunk) => console.log(chunk));
