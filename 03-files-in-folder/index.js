25094ddaff29a4937a612145c225eac6dfa0e642
leonid Kozlovskiy <lenya.kozlovskiy.97@mail.ru>
2024-01-21 06:37:16 +0300

    feat:add check is this a file for task three

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -4,12 +4,14 @@ const { stdout } = process;
 const pathName = path.join(__dirname, 'secret-folder');
 fs.readdir(pathName, { withFileTypes: true }, (err, files) => {
   files.forEach((e) => {
-    const pathToFile = path.join(pathName, e.name);
-    const name = path.parse(pathToFile).name;
-    const ext = path.parse(pathToFile).ext;
-    let result = `${name} ${ext} `;
-    fs.stat(pathToFile, (err, stats) => {
-      stdout.write(`${result} ${stats.size / 1000}\n `);
-    });
+    if (e.isFile()) {
+      const pathToFile = path.join(pathName, e.name);
+      const name = path.parse(pathToFile).name;
+      const ext = path.parse(pathToFile).ext;
+      let result = `${name} ${ext} `;
+      fs.stat(pathToFile, (err, stats) => {
+        stdout.write(`${result} ${stats.size / 1000}\n `);
+      });
+    }
   });
 });
