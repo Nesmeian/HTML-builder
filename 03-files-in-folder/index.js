fc31fd6f5f61d6b6048a65b0e995ac2412539e72
leonid Kozlovskiy <lenya.kozlovskiy.97@mail.ru>
2024-01-21 06:29:31 +0300

    feat:finish task number three

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const fs = require('fs');
+const { stdout } = process;
+const pathName = path.join(__dirname, 'secret-folder');
+fs.readdir(pathName, { withFileTypes: true }, (err, files) => {
+  files.forEach((e) => {
+    const pathToFile = path.join(pathName, e.name);
+    const name = path.parse(pathToFile).name;
+    const ext = path.parse(pathToFile).ext;
+    let result = `${name} ${ext} `;
+    fs.stat(pathToFile, (err, stats) => {
+      stdout.write(`${result} ${stats.size / 1000}\n `);
+    });
+  });
+});
