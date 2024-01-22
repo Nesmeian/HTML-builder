e47d1faa56114cfda47b85aa4ba6bccc019b4b38
leonid Kozlovskiy <lenya.kozlovskiy.97@mail.ru>
2024-01-22 06:20:18 +0300

    feat:add solution for task number five

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,27 +1,28 @@
 const path = require('path');
-const fs = require('fs');
+const fs = require('fs').promises;
 const pathName = path.join(__dirname, 'styles');
 const pathCopy = path.join(__dirname, 'project-dist');
 const pathBundle = path.join(pathCopy, 'bundle.css');
+
 async function merge() {
   try {
-    fs.mkdir(pathCopy, { recursive: true }, (err) => {
+    await fs.mkdir(pathCopy, { recursive: true });
+    const files = await fs.readdir(pathName);
+    const cssFiles = files.filter((file) => path.extname(file) === '.css');
+    let styles = '';
+    for (const file of cssFiles) {
+      const filePath = path.join(pathName, file);
+      const fileStyles = await fs.readFile(filePath, 'utf-8');
+      styles += fileStyles;
+    }
+    fs.writeFile(pathBundle, styles, (err) => {
       if (err) {
         console.log(err);
       }
-      fs.readdir(pathName, (err, files) => {
-        if (err) {
-          console.log(err);
-        }
-        files.forEach((e) => {
-          console.log(e);
-        });
-      });
     });
   } catch (err) {
-    if (err) {
-      console.log(err);
-    }
+    console.error(err);
   }
 }
+
 merge();
