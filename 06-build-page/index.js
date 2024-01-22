8a0f7421203b831fca5dfb212843f45f6dea38a1
leonid Kozlovskiy <lenya.kozlovskiy.97@mail.ru>
2024-01-23 01:35:50 +0300

    feat:complete task six

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,29 +1,67 @@
-const fs = require('fs');
+const fs = require('fs').promises;
+const { mkdir } = require('fs');
 const path = require('path');
-const components = path.join(__dirname, 'components');
-const styles = path.join(__dirname, 'styles');
-const createDir = path.join(__dirname, 'project-dist');
-const templateHtml = path.join(__dirname, 'template.html');
 
-console.log(templateHtml);
-function createHtml() {
-  fs.mkdir(createDir, { recursive: true }, (err) => {
+const projectLocate = path.join(__dirname, 'project-dist');
+const projectHtml = path.join(projectLocate, 'index.html');
+const components = path.join(__dirname, 'components');
+const styleFile = path.join(projectLocate, 'style.css');
+const templatePath = path.join(__dirname, 'template.html');
+const styles = path.join(__dirname, 'styles');
+const mainAssets = path.join(projectLocate, 'assets');
+const assets = path.join(__dirname, 'assets');
+function makeDir() {
+  fs.mkdir(projectLocate, { recursive: true }, (err) => {
     if (err) {
       console.log(err);
     }
-  });
-  fs.readFile(templateHtml, (err, data) => {
-    if (err) {
-      console.log(err);
-    }
-    dataContent = data.toString();
-    fs.readdir(components, (err, files) => {
-      if (err) {
-        console.log(err);
-      }
-      files = files.filter((e) => path.extname(e) === '.html');
-    });
   });
 }
+async function makeHtml() {
+  const templateContent = await fs.readFile(templatePath, 'utf-8');
+  const tagRegex = /{{([^}]+)}}/g;
+  const tags = templateContent.match(tagRegex) || [];
 
-createHtml();
+  let modifiedTemplate = templateContent;
+
+  for (const tag of tags) {
+    const tagName = tag.slice(2, -2);
+    const componentPath = path.join(components, `${tagName}.html`);
+
+    const componentContent = await fs.readFile(componentPath, 'utf-8');
+    modifiedTemplate = modifiedTemplate.replace(tag, componentContent);
+  }
+  fs.writeFile(projectHtml, modifiedTemplate);
+}
+
+async function mergeStyles() {
+  const files = await fs.readdir(styles);
+  for (const style of files) {
+    const pathToStyle = path.join(styles, style);
+    const styleContnet = await fs.readFile(pathToStyle);
+    await fs.appendFile(styleFile, styleContnet);
+  }
+}
+
+async function copyAssets(mk1, mk2) {
+  const topLevel = await fs.readdir(mk1, { withFileTypes: true });
+  await fs.mkdir(mk2, { recursive: true });
+  for (let elem of topLevel) {
+    const path1 = path.join(mk1, elem.name);
+    const path2 = path.join(mk2, elem.name);
+    if (elem.isDirectory()) {
+      copyAssets(path1, path2);
+    } else {
+      fs.copyFile(path1, path2);
+    }
+  }
+}
+
+async function makePage() {
+  makeDir();
+  makeHtml();
+  mergeStyles();
+  copyAssets(assets, mainAssets);
+}
+
+makePage();
